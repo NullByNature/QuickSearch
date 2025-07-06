263ec328bdc9a560aae7f7e6e84b2e70d32c8e5f
NullByNature <austinplaylabs@gmail.com>
2025-07-05 21:08:39 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using QuickSearch;
+
+namespace QuickSearch
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Instance of program 
+            Program program = new Program();
+            // Actual program
+            Console.WriteLine("QuickSearch v1.0.0 by NullByNature");
+            Console.WriteLine("You can select from the following directories to search from, more will be added later on");
+            Console.WriteLine("1. Desktop Directory\n" +
+                "2. Documents Directory\n" +
+                "3. Downloads Directory\n");
+            Console.Write("Input: ");
+            // User input
+            string user_answer = Console.ReadLine();
+            // Make sure user input is not empty
+            while(string.IsNullOrEmpty(user_answer))
+            {
+                Console.WriteLine("Your answer can not be empty");
+                user_answer = Console.ReadLine();
+            }
+            // dir search 
+            await program.DirectorySearch(user_answer);
+        }
+
+        #region File methods
+
+
+        async Task DirectorySearch(string directory)
+        {
+            DirSearch dir = new DirSearch();
+            switch (directory)
+            {
+                case "1":
+                    string Desktop_Path = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+                    Console.WriteLine("Enter the keyword you are looking for");
+                    Console.Write("Keyword: ");
+                    string downloads_keyword = Console.ReadLine();
+                    // make sure keyword is not empty 
+                    while (string.IsNullOrEmpty(downloads_keyword))
+                    {
+                        Console.WriteLine("Your keyword search can not be left blank");
+                        Console.Write("Keyword: ");
+                        downloads_keyword = Console.ReadLine();
+                    }
+                    await dir.Keyword(downloads_keyword, Desktop_Path);
+                    break;
+                case "2":
+                    string Documents_Path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                    Console.WriteLine("Enter the keyword you are looking for");
+                    Console.Write("Keyword: ");
+                    string Documents_keyword = Console.ReadLine();
+                    // make sure keyword is not empty 
+                    while (string.IsNullOrEmpty(Documents_keyword))
+                    {
+                        Console.WriteLine("Your keyword search can not be left blank");
+                        Console.Write("Keyword: ");
+                        Documents_keyword = Console.ReadLine();
+                    }
+                    await dir.Keyword(Documents_keyword, Documents_Path);
+                    break;
+                case "3":
+                    string Downloads_Path = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+                    Console.WriteLine("Enter the keyword you are looking for");
+                    Console.Write("Keyword: ");
+                    string Downloads_keyword = Console.ReadLine();
+                    // make sure keyword is not empty 
+                    while (string.IsNullOrEmpty(Downloads_keyword))
+                    {
+                        Console.WriteLine("Your keyword search can not be left blank");
+                        Console.Write("Keyword: ");
+                        Downloads_keyword = Console.ReadLine();
+                    }
+                    await dir.Keyword(Downloads_keyword, Downloads_Path);
+                    break;
+            }
+        }
+        #endregion
+    }
+}
---
+++ dirSearch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
+
+namespace QuickSearch
+{
+    class DirSearch
+    {
+        public async Task Keyword(string keyword, string DirPath)
+        {
+            // List for saving all files that contain the keyword 
+            List<string> keyword_file = new List<string>();
+            string path = DirPath;
+            // Get all files in the folder
+            string[] files = Directory.GetFiles(path);
+            // Loop through and read every file
+            DirectoryInfo directoryInfo = new DirectoryInfo(path);
+            // Try - Catch to prevent crashing 
+            try
+            {
+                foreach (string file in files)
+                {
+                    // read all text in each file 
+                    string readText = await File.ReadAllTextAsync(file);
+                    if (readText.Contains(keyword))
+                    {
+                        string name = Path.GetFileName(file);
+                        keyword_file.Add(name);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            // Check if keyword list has any files in it 
+            if (keyword_file.Count == 0)
+            {
+                Console.WriteLine("No files found with that keyword");
+            }
+            else
+            {
+                // Loop through every file name in keyword files
+                foreach (string file in keyword_file)
+                {
+                    Console.WriteLine($"Keyword found in file: {file}");
+                }
+            } 
+        }
+    }
+}
